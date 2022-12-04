56bac1f4d29b8b09f8ff0158a83bd5a3fcb80848
maximiliann97 <maximilian@live.se>
2022-12-04 11:14:29 +0100

    Update Sparrow.py

--- python_proj/Sparrow.py
+++ python_proj/Sparrow.py
@@ -2,15 +2,16 @@ import numpy as np
 
 
 class Sparrow:
-    def __init__(self, grid: int, age: int, hungry: bool, alive: bool):
-        if isinstance(grid, int):
-            self.position = np.random.randint(1, grid+1)
+    def __init__(self, N: int, age: int, hungry: bool, alive: bool):
+        if isinstance(N, int):
+            self.position = np.random.randint(1, N+1)
         else:
             raise TypeError('Needs to take integer')
         
         self.age = age
         self.hungry = hungry
         self.alive = alive
+        self.days_without_food = 0
         
     def update_hungry(self, hungry):
         if isinstance(hungry, bool):
@@ -25,7 +26,17 @@ class Sparrow:
             raise TypeError('Needs to take boolean')
         
         
-    def move(self):
+    def move(self, direction, grid):
+        return 0
 
+    def age(self, day):
+        self.age += day
 
-    def age(self):
+    def food(self, food):
+        if food:
+            self.update_hungry(False)
+            self.days_without_food = 0
+        else:
+            self.update_hungry(True)
+            self.days_without_food += 1
+
