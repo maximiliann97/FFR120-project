cfe5543ce931f199c5d67f21f1e187d1ba1302a4
maximiliann97 <maximilian@live.se>
2022-12-04 10:59:05 +0100

    Sparrow class

--- python_proj/Sparrow.py
+++ python_proj/Sparrow.py
@@ -1,5 +1,31 @@
 import numpy as np
 
+
 class Sparrow:
-    def __init__(self, grid):
+    def __init__(self, grid: int, age: int, hungry: bool, alive: bool):
+        if isinstance(grid, int):
+            self.position = np.random.randint(1, grid+1)
+        else:
+            raise TypeError('Needs to take integer')
         
+        self.age = age
+        self.hungry = hungry
+        self.alive = alive
+        
+    def update_hungry(self, hungry):
+        if isinstance(hungry, bool):
+            self.hungry = hungry
+        else:
+            raise TypeError('Needs to take boolean')
+        
+    def update_alive(self, alive):
+        if isinstance(alive, bool):
+            self.alive = alive
+        else:
+            raise TypeError('Needs to take boolean')
+        
+        
+    def move(self):
+
+
+    def age(self):
