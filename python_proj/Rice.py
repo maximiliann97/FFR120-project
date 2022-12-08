a645be275809f29010eda9ab15f18c7542584b37
maximiliann97 <maximilian@live.se>
2022-12-08 11:02:42 +0100

    rice update

--- python_proj/Rice.py
+++ python_proj/Rice.py
@@ -1,12 +1,18 @@
 import numpy as np
 
 class Rice:
-    def __init__(self, fields, N):
-        self.center = np.array([N/2,N/2])
-        self.upper_left = np.array([0,0])
-        self.upper_right = np.array([0, N-1])
-        self.lower_left = np.array([N-1,0])
-        self.lower_right = np.array([N-1,N-1])
+    def __init__(self, N, amount_rice):
+        center = np.array([N//2, N//2, amount_rice])
+        upper_left = np.array([0, 0, amount_rice])
+        upper_right = np.array([0, N-1, amount_rice])
+        lower_left = np.array([N-1, 0, amount_rice])
+        lower_right = np.array([N-1, N-1, amount_rice])
+        temp_list = [center, upper_left, upper_right, lower_left, lower_right]
+        self.fields = np.zeros([len(temp_list), 3])
+        for index, element in enumerate(temp_list):
+            self.fields[index, :] = element
+
+
         
 
     def grow_rice(self):
@@ -17,3 +23,8 @@ class Rice:
         self.fields[plant, -1] -= 2  # Rice gets eaten
         if self.fields[plant, -1] < 0:
             self.fields[plant, -1] = 0
+
+
+
+rice = Rice(10, 20)
+print(rice.fields)
