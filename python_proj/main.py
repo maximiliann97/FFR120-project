659da30931b52974d1c9223b101f963bdcd6116e
Axelqv <Axel.qvarnstrom@gmail.com>
2022-12-08 17:44:58 +0100

    update

--- python_proj/main.py
+++ python_proj/main.py
@@ -12,27 +12,32 @@ def main():
     lattice_size = 100
     nSparrows = 100
     nInsects = 50
-    sparrow = [Sparrow(lattice_size) for _ in range(nSparrows)]
+    sparrows = [Sparrow(lattice_size) for _ in range(nSparrows)]
     insects = [Insects(lattice_size) for _ in range(nInsects)]
     rice = Rice(lattice_size, 20)
     rice_field = rice.fields
     rice_coords = rice_field[:, 0:2]
 
     for t in range(timesteps):
-        for bird in sparrow:        # Birds move
-            bird.move(rice_field[:, 0:2])
+        for bird in sparrows:        # Birds move
+            bird.move(rice_coords)
         # for insect in insects:
         #     insect.move(rice_field[:, 0:2])
-        for bird in sparrow:        # Birds eat
+        for bird in sparrows:        # Birds eat
             true_array = np.all(bird.position == rice_coords, axis=1)
-            if True in true_array:
+            if True in true_array:  # If bird is at rice field
+                bird.food(True)
                 rice_field_row = np.where(true_array == True)[0][0]
                 rice.rice_gets_eaten(rice_field_row)
+            if not bird.alive:
+                sparrows.remove(bird)   # bird dies
+
 
         # Insects eat
         rice.grow_rice()
-        n_birthed_birds = np.floor(len(sparrow) * 0.1)
-        sparrow += [Sparrow(lattice_size) for x in range(n_birthed_birds)]
+        n_birthed_birds = np.floor(len(sparrows) * 0.01).astype(int)
+        sparrows += [Sparrow(lattice_size) for _ in range(n_birthed_birds)]
+        print(t)
 
 if __name__ == '__main__':
     main()
