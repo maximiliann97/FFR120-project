f0b06c5365b348ba03174da98bb492de4bfa0048
Axelqv <Axel.qvarnstrom@gmail.com>
2022-12-08 10:35:34 +0100

    change rise

--- python_proj/Rice.py
+++ python_proj/Rice.py
@@ -1,6 +1,13 @@
+import numpy as np
+
 class Rice:
-    def __init__(self, fields):
-        self.fields = fields
+    def __init__(self, fields, N):
+        self.center = np.array([N/2,N/2])
+        self.upper_left = np.array([0,0])
+        self.upper_right = np.array([0, N-1])
+        self.lower_left = np.array([N-1,0])
+        self.lower_right = np.array([N-1,N-1])
+        
 
     def grow_rice(self):
         self.fields[:, -1] += 1         # Adds one rice grain per day
--- python_proj/animation.py
+++ python_proj/animation.py
@@ -19,21 +19,21 @@ def animate(i):
     #sparrow.append(Sparrow(lattice_size))
     #Insects.append(insects(lattice_size))
     for bird in sparrow:
-        plt.scatter(bird.position[0],bird.position[1],c='b')
+        plt.scatter(bird.position[0], bird.position[1], c='b')
  
-    x.append(random.randint(2,lattice_size))
-    y.append(random.randint(2,lattice_size))
+    x.append(random.randint(2, lattice_size))
+    y.append(random.randint(2, lattice_size))
     lastx = x.pop()
     lasty = y.pop()
-    for i in range(100) :
-        x.append(lastx+random.randint(1,3))
-        y.append(lasty+random.randint(1,3))
-        #plt.style.use("ggplot")    
-        plt.scatter(x,y,c='g')
+    # for i in range(100):
+    #     x.append(lastx+random.randint(1,3))
+    #     y.append(lasty+random.randint(1,3))
+    #     #plt.style.use("ggplot")
+    #     plt.scatter(x,y,c='g')
     #plt.scatter(sparrow.position,c='b')
     #plt.scatter(Insects.position,c='r')
 #test code to include obj
 sparrow.append(Sparrow(lattice_size))
 #Insects.append(insects(lattice_size))
-ani = FuncAnimation(fig, animate, interval=300)
+ani = FuncAnimation(fig, animate, interval=30)
 plt.show()
