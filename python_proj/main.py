686e3d7817488691308fa20f4716f54c0e4d77fa
maximiliann97 <maximilian@live.se>
2022-12-08 16:19:28 +0100

    beginning of rice gets eaten
    Tries to get sparrow to eat rice correctly

--- python_proj/main.py
+++ python_proj/main.py
@@ -1,16 +1,31 @@
+import random
 import matplotlib.pyplot as plt
 import numpy as np
-import random
 
-def main():  
-    t = 0
-    while t< 100 :
-        X = random.sample(range(1, 100), 10)
-        Y = random.sample(range(1, 100), 10)
-        plt.hold(True)
-        plt.scatter(X, Y)
-        plt.show()
-        plt.hold(False)
+from Sparrow import Sparrow
+from Insects import Insects
+from Rice import Rice
+
+def main():
+
+    timesteps = 200
+    lattice_size = 100
+    nSparrows = 100
+    nInsects = 50
+    sparrow = [Sparrow(lattice_size) for _ in range(nSparrows)]
+    insects = [Insects(lattice_size) for _ in range(nInsects)]
+    rice = Rice(lattice_size, 20)
+    rice_field = rice.fields
+    rice_coords = rice_field[:, 0:2]
+
+    for t in range(timesteps):
+        for bird in sparrow:
+            bird.move(rice_field[:, 0:2])
+            true_array = np.all(bird.position == rice_coords, axis=1)
+            if True in true_array:
+                rice.rice_gets_eaten(bird.position)
+
+
 
 
 
