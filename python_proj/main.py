605f807b342e4706220097700aebab8aaa211b99
maximiliann97 <maximilian@live.se>
2022-12-10 19:57:17 +0100

    Update main.py

--- python_proj/main.py
+++ python_proj/main.py
@@ -2,6 +2,7 @@ import ecosystem_simulation as ecosystem
 import animation_file as animation
 import matplotlib.pyplot as plt
 
+
 def main():
     ecosystem.run_simulation()
     animation.run_animation()
